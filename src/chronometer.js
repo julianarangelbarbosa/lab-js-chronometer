7ff24f9383afca6fbbb436a0da0d9ab8a5842e3d
julianarangelbarbosa <juliana.rangel.barbosa@gmail.com>
2022-10-10 16:34:01 +0100

    Labs 6

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,64 @@
-class Chronometer {
-  constructor() {
-    // ... your code goes here
+class Chronometer 
+
+{
+  constructor(currentTime, intervalId) {
+    this.currentTime = 0;
+    this.intervalId = null;
+
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(() => {
+       this.currentTime ++;
+       if (printTimeCallback) 
+       {
+           printTimeCallback();
+       }
+      },1000);
+    };
+   
 
-  getMinutes() {
-    // ... your code goes here
-  }
+  getMinutes() 
+  {
+    return Math.floor(this.currentTime / 60); 
+  } 
 
-  getSeconds() {
-    // ... your code goes here
+  getSeconds() {    
+    return Math.floor(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10){
+    return `0${value}`;
+    }
+    else {
+      return `${value}`;
+    }
   }
 
-  stop() {
-    // ... your code goes here
+  stop() 
+  {
+ return clearInterval(this.currentTime);
   }
 
   reset() {
-    // ... your code goes here
+    return  this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
-  }
+  
+return `${this.computeTwoDigitNumber(this.getMinutes())}` + ":" + `${this.computeTwoDigitNumber(this.getSeconds())}`  
+  
+
+  
+
+
+  //  let hh = this.getMinutes;
+   // let mm = this.getSeconds
+  //  if (hh < 10 && mm >=10 ) {return `0${hh} : {mm}`; }
+   // else if (hh < 10 && mm <10 ) {return `0${hh} : 0${mm}`; }
+  //  else if (hh > 10 && mm <10 ) {return `${hh} : 0${mm}`; }
+   // else if (hh > 10 && mm >10 ) {return `${hh} : ${mm}`; }
 }
+}
+
